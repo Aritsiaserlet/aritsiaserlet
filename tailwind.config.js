01409de09df5e57ed21459f103e5453d015e8126
OzonZ <chanonbanana@gmail.com>
2026-06-14 16:16:02 +0700

    2.5.0

---
+++ tailwind.config.js
@@ -0,0 +1,163 @@
+module.exports = {
+  "darkMode": "class",
+  "content": [
+    "./*.html",
+    "./js/**/*.js"
+  ],
+  "theme": {
+    "extend": {
+      "colors": {
+        "primary": "var(--primary)",
+        "on-primary": "var(--on-primary)",
+        "background": "var(--background)",
+        "on-background": "var(--on-background)",
+        "surface": "var(--surface)",
+        "on-surface": "var(--on-surface)",
+        "surface-variant": "var(--surface-variant)",
+        "on-surface-variant": "var(--on-surface-variant)",
+        "outline": "var(--outline)",
+        "secondary": "var(--secondary)",
+        "on-secondary": "var(--on-secondary)",
+        "tertiary": "var(--tertiary)",
+        "on-primary-container": "var(--on-primary-container)",
+        "tertiary-fixed-dim": "var(--tertiary-fixed-dim)",
+        "inverse-on-surface": "var(--inverse-on-surface)",
+        "surface-dim": "var(--surface-dim)",
+        "inverse-primary": "var(--inverse-primary)",
+        "on-primary-fixed": "var(--on-primary-fixed)",
+        "surface-container-high": "var(--surface-container-high)",
+        "on-tertiary": "var(--on-tertiary)",
+        "secondary-fixed": "var(--secondary-fixed)",
+        "on-secondary-fixed-variant": "var(--on-secondary-fixed-variant)",
+        "error-container": "var(--error-container)",
+        "on-error": "var(--on-error)",
+        "surface-bright": "var(--surface-bright)",
+        "surface-container-low": "var(--surface-container-low)",
+        "surface-container": "var(--surface-container)",
+        "on-error-container": "var(--on-error-container)",
+        "secondary-fixed-dim": "var(--secondary-fixed-dim)",
+        "secondary-container": "var(--secondary-container)",
+        "surface-tint": "var(--surface-tint)",
+        "primary-fixed-dim": "var(--primary-fixed-dim)",
+        "tertiary-container": "var(--tertiary-container)",
+        "tertiary-fixed": "var(--tertiary-fixed)",
+        "outline-variant": "var(--outline-variant)",
+        "inverse-surface": "var(--inverse-surface)",
+        "surface-container-highest": "var(--surface-container-highest)",
+        "on-secondary-container": "var(--on-secondary-container)",
+        "error": "var(--error)",
+        "on-tertiary-container": "var(--on-tertiary-container)",
+        "on-secondary-fixed": "var(--on-secondary-fixed)",
+        "primary-container": "var(--primary-container)",
+        "on-primary-fixed-variant": "var(--on-primary-fixed-variant)",
+        "primary-fixed": "var(--primary-fixed)",
+        "on-tertiary-fixed": "var(--on-tertiary-fixed)",
+        "surface-container-lowest": "var(--surface-container-lowest)",
+        "on-tertiary-fixed-variant": "var(--on-tertiary-fixed-variant)"
+      },
+      "borderRadius": {
+        "DEFAULT": "0.25rem",
+        "lg": "0.5rem",
+        "xl": "0.75rem",
+        "full": "9999px"
+      },
+      "spacing": {
+        "margin-mobile": "16px",
+        "max-width": "1280px",
+        "gutter": "24px",
+        "unit": "8px",
+        "margin-desktop": "64px",
+        "stack-md": "16px",
+        "stack-lg": "32px",
+        "stack-sm": "8px",
+        "margin-x": "32px",
+        "container-max": "1440px"
+      },
+      "fontFamily": {
+        "headline": [
+          "Mitr",
+          "sans-serif"
+        ],
+        "body-md": [
+          "Space Grotesk"
+        ],
+        "display-lg": [
+          "Mitr"
+        ],
+        "label-sm": [
+          "Space Grotesk",
+          "sans-serif"
+        ],
+        "headline-md": [
+          "Mitr"
+        ],
+        "data-mono": [
+          "Space Grotesk"
+        ],
+        "label-md": [
+          "Space Grotesk"
+        ],
+        "headline-lg": [
+          "Mitr"
+        ],
+        "body-lg": [
+          "Space Grotesk"
+        ]
+      },
+      "fontSize": {
+        "headline-md": [
+          "24px",
+          {
+            "lineHeight": "1.4",
+            "fontWeight": "400"
+          }
+        ],
+        "body-md": [
+          "16px",
+          {
+            "lineHeight": "1.6",
+            "fontWeight": "400"
+          }
+        ],
+        "data-mono": [
+          "14px",
+          {
+            "lineHeight": "1",
+            "letterSpacing": "-0.01em",
+            "fontWeight": "600"
+          }
+        ],
+        "label-md": [
+          "14px",
+          {
+            "lineHeight": "1.2",
+            "letterSpacing": "0.05em",
+            "fontWeight": "500"
+          }
+        ],
+        "headline-lg": [
+          "32px",
+          {
+            "lineHeight": "1.3",
+            "fontWeight": "500"
+          }
+        ],
+        "display-lg": [
+          "48px",
+          {
+            "lineHeight": "1.2",
+            "letterSpacing": "-0.02em",
+            "fontWeight": "500"
+          }
+        ],
+        "body-lg": [
+          "18px",
+          {
+            "lineHeight": "1.6",
+            "fontWeight": "400"
+          }
+        ]
+      }
+    }
+  }
+};
